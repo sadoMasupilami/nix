b57662aa650bd7ffda4ae1d7d8d7fd78c54bcbd7
Michael Klug <michiklug85@gmail.com>
2024-10-30 20:59:14 +0100

    trying

--- modules/shared/default.nix
+++ modules/shared/default.nix
@@ -19,11 +19,6 @@ in
       map (n: import (path + ("/" + n)))
           (filter (n: match ".*\\.nix" n != null ||
                       pathExists (path + ("/" + n + "/default.nix")))
-                  (attrNames (readDir path)))
-
-      ++ [(import (builtins.fetchTarball {
-               url = "https://github.com/dustinlyons/emacs-overlay/archive/refs/heads/master.tar.gz";
-               sha256 = emacsOverlaySha256;
-           }))];
+                  (attrNames (readDir path)));
   };
 }
