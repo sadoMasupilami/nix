ed8ed9c9bb27db2bc10c82ef5937495039c16a44
Michael Klug <michael.klug@fullstacks.eu>
2024-10-31 15:52:57 +0100

    fix: disk naming

--- modules/nixos/disk-config.nix
+++ modules/nixos/disk-config.nix
@@ -1,17 +1,17 @@
 _: {
   fileSystems."/" =
-    { device = "/dev/disk/by-uuid/1e72b089-a6a8-4b14-9548-80c88cdb2e5b";
+    { device = "/dev/disk/by-label/ROOT";
       fsType = "ext4";
     };
 
   fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/4F6E-67F4";
+    { device = "/dev/disk/by-label/BOOT";
       fsType = "vfat";
       options = [ "fmask=0077" "dmask=0077" ];
     };
 
   swapDevices =
-    [ { device = "/dev/disk/by-uuid/9625b394-4d34-409b-a50d-d5079e41d044"; }
+    [ { device = "/dev/disk/by-label/SWAP"; }
     ];
 #  # This formats the disk with the ext4 filesystem
 #  # Other examples found here: https://github.com/nix-community/disko/tree/master/example
---
+++ scripts/install.sh
@@ -0,0 +1,39 @@
+# Replace /dev/sdX with your actual target disk
+DISK="/dev/nvme0n1"
+
+# Create a new GPT partition table
+parted $DISK --script mklabel gpt
+
+# Create a 512MB EFI system partition
+parted $DISK --script mkpart primary fat32 1MiB 513MiB
+parted $DISK --script set 1 esp on  # Mark as EFI System Partition
+
+# Create a root partition with remaining space
+parted $DISK --script mkpart primary ext4 513MiB 96%
+
+# Optionally create a 4GB swap partition at the end (adjust as needed)
+parted $DISK --script mkpart primary linux-swap 96% 100%
+
+# EFI partition
+mlabel -i /dev/nvme0n1p1 ::BOOT
+mkfs.fat -F 32 ${DISK}p1
+
+# root partition
+e2label /dev/nvme0n1p2 ROOT
+mkfs.ext4 ${DISK}p2 -F
+
+# Format and enable swap
+mkswap -L SWAP ${DISK}p3
+swapon ${DISK}p3
+
+udevadm trigger
+
+mount ${DISK}p2 /mnt       # Mount root partition
+mkdir -p /mnt/boot
+mount ${DISK}p1 /mnt/boot  # Mount EFI partition
+swapon ${DISK}p3
+
+git clone https://github.com/sadoMasupilami/nix.git
+cd nix || exit
+
+nixos-install --root /mnt --flake ./#aarch64-linux
