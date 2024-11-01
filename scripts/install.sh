51f7981c704a53db1ecf565d32d5a3ac3de0b6b0
Michael Klug <michael.klug@fullstacks.eu>
2024-11-01 09:01:38 +0100

    feat: finally working

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env nix-shell
 #!nix-shell -i bash -p git
-set -e
 
-# git is needed in nixos-minimal
-nix-shell -p git
 # Replace /dev/sdX with your actual target disk
 DISK="/dev/nvme0n1"
+
+# wipe the disk, you should only do this if you are sure it is empty
+wipefs -a ${DISK}
 
 # Create a new GPT partition table
 parted $DISK --script mklabel gpt
@@ -14,30 +14,30 @@ parted $DISK --script mklabel gpt
 parted $DISK --script mkpart primary fat32 1MiB 513MiB
 parted $DISK --script set 1 esp on  # Mark as EFI System Partition
 
-# Create a root partition with remaining space
+# Create a root partition with most space
 parted $DISK --script mkpart primary ext4 513MiB 96%
 
-# Optionally create a 4GB swap partition at the end (adjust as needed)
+# Create a swap partition at the end (adjust as needed)
 parted $DISK --script mkpart primary linux-swap 96% 100%
 
 # EFI partition
-mlabel -i /dev/nvme0n1p1 ::BOOT
 mkfs.fat -F 32 ${DISK}p1
+mlabel -i /dev/nvme0n1p1 ::BOOT
 
 # root partition
-e2label /dev/nvme0n1p2 ROOT
 mkfs.ext4 ${DISK}p2 -F
+e2label /dev/nvme0n1p2 ROOT
 
 # Format and enable swap
 mkswap -L SWAP ${DISK}p3
 
 # populate /dev/disk/by-label, not sure if needed
-#udevadm trigger
+udevadm trigger
 
 mount ${DISK}p2 /mnt       # Mount root partition
 mkdir -p /mnt/boot
 mount ${DISK}p1 /mnt/boot  # Mount EFI partition
-swapon ${DISK}p3
+swapon ${DISK}p3           # Enable swap
 
 git clone https://github.com/sadoMasupilami/nix.git
 cd nix || exit
